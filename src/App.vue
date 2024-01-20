21153122c12a979700ef4b18128dbfbac899aaa2
ChathuraSudaraka <Chathurabro68@gmail.com>
2024-01-20 22:05:12 +0530

    updated button

--- src/App.vue
+++ src/App.vue
@@ -32,14 +32,10 @@ const theme = computed(() => {
 </script>
 
 <template>
-  <div
-    class="flex min-h-screen justify-center bg-gray-100 dark:bg-[#141414] bg-[url(/img/1.png)] bg-cover bg-left-bottom"
-    :data-theme="theme"
-  >
+  <div class="flex min-h-screen justify-center bg-gray-100 dark:bg-[#141414] bg-[url(/img/1.png)] bg-cover bg-center"
+    :data-theme="theme">
     <div class="container p-5 pt-10">
-      <div
-        class="p-5 md:p-8 lg:p-10 rounded-lg shadow-lg bg-[#f8fafc99] dark:bg-[#0f0f12e3] dark:shadow-black"
-      >
+      <div class="p-5 md:p-8 lg:p-10 rounded-lg shadow-lg bg-[#f8fafc99] dark:bg-[#0f0f12e3] dark:shadow-black">
         <div class="grid grid-cols-12 gap-3">
           <!-- Trid ID Start -->
           <div class="col-span-12 lg:col-span-6">
@@ -57,35 +53,19 @@ const theme = computed(() => {
           <!-- Trid Date & Time end -->
 
           <!-- Passengers Start -->
-          <div
-            class="col-span-12 md:col-span-6 lg:col-span-4 grid grid-cols-2 gap-3"
-          >
+          <div class="col-span-12 md:col-span-6 lg:col-span-4 grid grid-cols-2 gap-3">
             <div class="col-span-1">
-              <PrimaryInput
-                type="number"
-                label="Adults"
-                placeholder="Adults Count"
-                keyboard="numeric"
-              />
+              <PrimaryInput type="number" label="Adults" placeholder="Adults Count" keyboard="numeric" />
             </div>
             <div class="col-span-1">
-              <PrimaryInput
-                type="number"
-                label="Children"
-                placeholder="Children Count"
-                keyboard="numeric"
-              />
+              <PrimaryInput type="number" label="Children" placeholder="Children Count" keyboard="numeric" />
             </div>
           </div>
           <!-- Passengers End -->
 
           <!-- Vehicle Type Start -->
           <div class="col-span-6 md:col-span-6 lg:col-span-4 flex flex-col">
-            <PrimarySelect
-              label="Vehicle Type"
-              placeholder="Select Vehicle Type"
-              :options="vehicles"
-            />
+            <PrimarySelect label="Vehicle Type" placeholder="Select Vehicle Type" :options="vehicles" />
           </div>
           <!-- Vehicle Type End -->
 
@@ -97,24 +77,16 @@ const theme = computed(() => {
 
           <!-- Location Start -->
           <div class="col-span-12 md:col-span-6">
-            <PrimaryInput
-              label="Pickup Location"
-              placeholder="Enter Pickup Location"
-            />
+            <PrimaryInput label="Pickup Location" placeholder="Enter Pickup Location" />
           </div>
           <div class="col-span-12 md:col-span-6">
-            <PrimaryInput
-              label="Drop Location"
-              placeholder="Enter Drop Location"
-            />
+            <PrimaryInput label="Drop Location" placeholder="Enter Drop Location" />
           </div>
           <!-- Location End -->
 
           <!-- Surfboard Start -->
           <div class="col-span-12 md:col-span-4">
-            <label for="surfboard" class="label text-sm uppercase"
-              >Surfboard</label
-            >
+            <label for="surfboard" class="label text-sm uppercase">Surfboard</label>
             <div class="grid grid-cols-2 place-items-center">
               <div class="flex items-center gap-3">
                 <input type="radio" name="surfboard" class="radio" />
@@ -130,42 +102,26 @@ const theme = computed(() => {
 
           <!-- Distance Start -->
           <div class="col-span-12 md:col-span-3">
-            <PrimaryInput
-              label="Total Distance"
-              placeholder="Enter The Distance"
-              messure="KM"
-            />
+            <PrimaryInput label="Total Distance" placeholder="Enter The Distance" messure="KM" />
           </div>
           <!-- Distance End -->
 
           <!-- Time Start -->
           <div class="col-span-12 md:col-span-5">
-            <label for="time" class="label uppercase text-sm"
-              >Transport Time</label
-            >
+            <label for="time" class="label uppercase text-sm">Transport Time</label>
             <div class="grid grid-cols-2 gap-3">
               <div class="col-span-1">
-                <PrimaryInput
-                  type="number"
-                  placeholder="Hours"
-                  keyboard="numeric"
-                  messure="H"
-                />
+                <PrimaryInput type="number" placeholder="Hours" keyboard="numeric" messure="H" />
               </div>
               <div class="col-span-1">
-                <PrimaryInput
-                  type="number"
-                  placeholder="Minutes"
-                  keyboard="numeric"
-                  messure="M"
-                />
+                <PrimaryInput type="number" placeholder="Minutes" keyboard="numeric" messure="M" />
               </div>
             </div>
           </div>
           <!-- Time End -->
 
           <!-- Button Set Start -->
-          <div class="col-span-12 flex justify-center lg:justify-end gap-3">
+          <div class="col-span-12 flex flex-col lg:flex-row justify-center lg:justify-end gap-4">
             <button class="btn btn-error">Reset</button>
             <button class="btn btn-primary">Print</button>
             <button class="btn btn-neutral">
