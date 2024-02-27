c1c199bd2af5d665f8f6e881139178e6273b392a
TharinduNimesh <127307898+TharinduNimesh@users.noreply.github.com>
2024-02-27 23:28:58 +0530

    new invoice design

--- src/components/Invoice.vue
+++ src/components/Invoice.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { jsPDF } from "jspdf";
 import Toastify from "toastify-js";
-import "../hook/Invoice.css"
 
 const props = defineProps({
   items: {
@@ -28,9 +27,6 @@ function print() {
 
   doc.html(printable, {
     callback: function (pdf) {
-      setTimeout(() => {
-        copy_icon.value = "md-contentcopy-round";
-      }, 1500);
       pdf.save(`transfer-${props.items.tripId}.pdf`);
     },
   });
@@ -46,148 +42,179 @@ function print() {
           ✕
         </button>
       </form>
-      <div class="flex content-center justify-center">
+      <div class="p-5">
         <!-- Invoice -->
         <div id="printable_invoice" class="">
-          <img src="/img/invoice-structure.png" class="absolute w-full" />
-          <div class="px-14 py-10" id="top-position">
-            <table class="w-full border-collapse border-spacing-0">
-              <tbody>
-                <tr>
-                  <td class="w-full align-top">
-                    <div>
-                      <img src="/img/machanlogo.png" id="logo" />
-                    </div>
-                  </td>
-
-                  <td class="align-top">
-                    <div class="text-sm">
-                      <table class="border-collapse border-spacing-0">
-                        <tbody>
-                          <tr>
-                            <td class="border-r pr-4">
-                              <div>
-                                <p class="whitespace-nowrap text-slate-400 text-right">Date</p>
-                                <p class="whitespace-nowrap font-bold text-main text-right">feb 26, 2024</p>
-                              </div>
-                            </td>
-                            <td class="pl-4">
-                              <div>
-                                <p class="whitespace-nowrap text-slate-400 text-right">Invoice #</p>
-                                <p class="whitespace-nowrap font-bold text-main text-right">BRA-00335</p>
-                              </div>
-                            </td>
-                          </tr>
-                        </tbody>
-                      </table>
-                    </div>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-
-          <div class="bg-slate-100 px-14 py-6 text-sm">
-            <table class="w-full border-collapse border-spacing-0">
-              <tbody>
-                <tr>
-                  <td class="w-1/2 align-top">
-                    <div class="text-sm text-neutral-600">
-                      <p>Phone: +94 705321516</p>
-                      <p>Email: example@gmail.com</p>
-                      <p>Addres: Example, 117 road</p>
-                    </div>
-                  </td>
-                  <td class="w-1/2 align-top text-right">
-                    <div class="text-sm text-neutral-600">
-                      <p class="font-bold">Customer Details</p>
-                      <p>Number: +94 705321516</p>
-                      <p>Date: 2024/12/55</p>
-                    </div>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-
-          <div class="px-14 py-10 text-sm text-neutral-700">
-            <table class="w-full border-collapse border-spacing-0">
-              <thead>
-                <tr>
-                  <td class="border-b-2 border-main pb-3 pl-3 font-bold text-main">Item</td>
-                  <td class="border-b-2 border-main pb-3 pl-2 font-bold text-main">Property</td>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td class="border-b py-3 pl-3">sssssssssss</td>
-                  <td class="border-b py-3 pl-2">services</td>
-                </tr>
-                <tr>
-                  <td colspan="7">
-                    <table class="w-full border-collapse border-spacing-0">
-                      <tbody>
-                        <tr>
-                          <td class="w-full"></td>
-                          <td>
-                            <table class="w-full border-collapse border-spacing-0">
-                              <tbody>
-                                <tr>
-                                  <td class="border-b p-3">
-                                    <div class="whitespace-nowrap text-slate-400">Net total:</div>
-                                  </td>
-                                  <td class="border-b p-3 text-right">
-                                    <div class="whitespace-nowrap font-bold text-main">Rs.320.00</div>
-                                  </td>
-                                </tr>
-                                <tr>
-                                  <td class="p-3">
-                                    <div class="whitespace-nowrap text-slate-400">VAT total:</div>
-                                  </td>
-                                  <td class="p-3 text-right">
-                                    <div class="whitespace-nowrap font-bold text-main">Rs.64.00</div>
-                                  </td>
-                                </tr>
-                                <tr>
-                                  <td class="bg-main p-3">
-                                    <div class="whitespace-nowrap font-bold text-white">Total:</div>
-                                  </td>
-                                  <td class="bg-main p-3 text-right">
-                                    <div class="whitespace-nowrap font-bold text-white">Rs.384.00</div>
-                                  </td>
-                                </tr>
-                              </tbody>
-                            </table>
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-
-          <div class="px-14 text-sm text-neutral-700" style="display: flex; flex-wrap: wrap;">
-            <div style="flex: 1; margin-right: 20px;">
-              <p class="text-main font-bold">THANK YOU FOR YOUR BUSINESS</p>
-              <span>
-                <v-icon name="Fa-Phone-Alt" />
-                <p>Phone</p>
-              </span>
-              <span>
-                <v-icon name="Fa-Phone-Alt" />
-                <p>Email</p>
-              </span>
-              <span>
-                <v-icon name="Fa-Phone-Alt" />
-                <p>Website</p>
-              </span>
+          <img src="/img/invoice-structure.jpg" class="absolute w-full" />
+          <div class="relative">
+            <div class="px-14 py-5" id="top-position">
+              <div class="grid grid-cols-2">
+                <div class="pt-2">
+                  <img src="/img/machanlogo.png" id="logo" />
+                </div>
+                <div class="flex flex-col pt-3">
+                  <div class="grid grid-cols-2">
+                    <div class="text-gray-800 font-bold">Invoice Id:</div>
+                    <div class="text-gray-500">BRA-00335</div>
+                  </div>
+                  <div class="grid grid-cols-2">
+                    <div class="text-gray-800 font-bold">Invoiced Date:</div>
+                    <div class="text-gray-500">feb 26, 2024</div>
+                  </div>
+                  <div class="grid grid-cols-2">
+                    <div class="text-gray-800 font-bold">Due Date :</div>
+                    <div class="text-gray-500">feb 30, 2024</div>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div class="text-right px-2 py-20 text-sm text-neutral-700" style="flex: 1;">
-              <p>__________________________</p>
-              <p class="text-main font-bold pr-12">NAME</p>
-              <p class="italic pr-6">COMPANY CEO</p>
+
+            <div
+              class="grid grid-cols-3 bg-orange-100 text-gray-700 px-14 py-3 text-sm"
+            >
+              <div class="col-span-2 flex-col gap-1">
+                <p><strong>Phone:</strong> +94 705321516</p>
+                <p><strong>Email:</strong> example@gmail.com</p>
+                <p><strong>Addres:</strong> Example, 117 road</p>
+              </div>
+              <div class="flex flex-col gap-1">
+                <p class="font-bold uppercase text-gray-700">
+                  Customer Details
+                </p>
+                <p><strong>Name:</strong> Chathura Sudaraka</p>
+                <p><strong>Number:</strong> +94 705321516</p>
+              </div>
+            </div>
+
+            <div class="px-14 py-5 text-sm text-neutral-700">
+              <table class="w-full border-collapse border-spacing-0">
+                <thead class="bg-[#ff5801] text-white">
+                  <tr>
+                    <td class="font-bold uppercase text-md px-5 py-2">Label</td>
+                    <td class="font-bold uppercase text-md px-5 py-2">Value</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td class="border-b py-3 pl-3">Trip Id</td>
+                    <td class="border-b py-3 pl-2">120120</td>
+                  </tr>
+                  <tr>
+                    <td class="border-b py-3 pl-3">Date & Time</td>
+                    <td class="border-b py-3 pl-2">2024 Dec 12, 10:00 AM</td>
+                  </tr>
+                  <tr>
+                    <td class="border-b py-3 pl-3">Passengers</td>
+                    <td class="border-b py-3 pl-2">
+                      <div class="w-full grid grid-cols-2">
+                        <div class="flex gap-2">
+                          <div class="text-gray-600">Adults:</div>
+                          <div class="text-gray-500">5</div>
+                        </div>
+                        <div class="flex gap-2">
+                          <div class="text-gray-600">Children:</div>
+                          <div class="text-gray-500">2</div>
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="border-b py-3 pl-3">Vehicle Type</td>
+                    <td class="border-b py-3 pl-2">Buddy Van</td>
+                  </tr>
+                  <tr>
+                    <td class="border-b py-3 pl-3">Baggages</td>
+                    <td class="border-b py-3 pl-2">5</td>
+                  </tr>
+                  <tr>
+                    <td class="border-b py-3 pl-3">Pickup Location</td>
+                    <td class="border-b py-3 pl-2">120120</td>
+                  </tr>
+                  <tr>
+                    <td class="border-b py-3 pl-3">Drop Location</td>
+                    <td class="border-b py-3 pl-2">120120</td>
+                  </tr>
+                  <tr>
+                    <td class="border-b py-3 pl-3">Total Distance</td>
+                    <td class="border-b py-3 pl-2">120120</td>
+                  </tr>
+                  <tr>
+                    <td class="border-b py-3 pl-3">Transport Time</td>
+                    <td class="border-b py-3 pl-2">5H 23M</td>
+                  </tr>
+                </tbody>
+              </table>
+              <div class="grid grid-cols-12 mt-3">
+                <div class="col-span-7 flex flex-col">
+                  <h3 class="text-[#ff5801] text-lg font-bold">
+                    THANK YOU FOR YOUR BUSINESS
+                  </h3>
+                  <div class="flex flex-col px-5 gap-2 mt-2">
+                    <span class="flex gap-3">
+                      <div
+                        class="p-[5px] bg-[#ff5801] rounded-full flex justify-center items-center"
+                      >
+                        <img 
+                          src="/img/icons/phone.png"
+                          class="w-[13px]"
+                        />
+                      </div>
+                      <p>+94 77 111 2223</p>
+                    </span>
+                    <span class="flex gap-3">
+                      <div
+                        class="p-[5px] bg-[#ff5801] rounded-full flex justify-center items-center"
+                      >
+                      <img 
+                          src="/img/icons/email.png"
+                          class="w-[13px]"
+                        />
+                      </div>
+                      <p>johndoe@example.com</p>
+                    </span>
+                  </div>
+                  <p class="mt-3">
+                    Please send payment within 30 days of receiving this
+                    invoice. There will be 10% interest charge per month on late
+                    invoice.
+                  </p>
+                </div>
+                <div class="col-span-5 grid grid-cols-2 p-3">
+                  <div
+                    class="border-t border-t-gray-300 col-span-full grid grid-cols-2 p-3"
+                  >
+                    <div class="font-bold">Sub Total:</div>
+                    <div class="">Rs. 800.00</div>
+                  </div>
+                  <div
+                    class="border-t border-t-gray-300 col-span-full grid grid-cols-2 p-3"
+                  >
+                    <div class="font-bold">Net Vat:</div>
+                    <div class="">Rs. 200.00</div>
+                  </div>
+                  <div class="bg-[#ff5801] col-span-full grid grid-cols-2 p-3">
+                    <div class="whitespace-nowrap font-bold text-white">
+                      Total:
+                    </div>
+                    <div class="whitespace-nowrap font-bold text-white">
+                      Rs. 1000.00
+                    </div>
+                  </div>
+                  <div class="col-span-full flex flex-col items-center mt-3">
+                    <div
+                      class="w-full border-b border-b-gray-400 border-dashed flex justify-center pb-2"
+                    >
+                      <img
+                        src="/img/signature.png"
+                        alt="signature"
+                        class="h-10"
+                      />
+                    </div>
+                    <p class="text-[#ff5801] mt-2 italic">John Doe</p>
+                    <p class="font-bold">COMPANY CEO</p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -210,10 +237,6 @@ function print() {
 </template>
 
 <style>
-body {
-  background: rgb(204, 204, 204);
-}
-
 #printable_invoice {
   position: relative;
   width: 795px;
@@ -228,15 +251,6 @@ body {
 }
 
 #logo {
-  height: 100px;
-}
-
-@media print {
-
-  body,
-  page[size="a4"] {
-    margin: 0;
-    box-shadow: 0;
-  }
+  height: 70px;
 }
 </style>
